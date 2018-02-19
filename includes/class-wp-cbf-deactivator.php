b996f3df4f00dcc35ad8adeb72dbdd015b1c0377
Surfing-Chef <surfing-chef@surfing-chef.com>
2018-02-19 15:54:19 -0800

    First commit

---
+++ includes/class-wp-cbf-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://www.surfing-chef.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Cbf
+ * @subpackage Wp_Cbf/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Cbf
+ * @subpackage Wp_Cbf/includes
+ * @author     Surfing Chef <surfing-chef@surfing-chef.com>
+ */
+class Wp_Cbf_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
